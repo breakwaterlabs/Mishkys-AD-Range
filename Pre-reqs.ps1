fbcadb1256977303f54157c9e9a0a85a8b2786f8
breakwaterlabs <John@breakwaterlabs.net>
2025-02-25 15:17:33 -0500

    Hyper-V needs to be installed first to enable new-vmswitch command to succeed

--- Pre-reqs.ps1
+++ Pre-reqs.ps1
@@ -1,6 +1,8 @@
 ﻿$VMStuff="C:\VM_Stuff_Share"
 $IsoSavePath = "$VMStuff\ISOs\Windows Server 2022 (20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us).iso"
 
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
 New-VMSwitch -Name "Testing" -NetAdapterName "Ethernet" ; Set-VMSwitch -Name Testing -AllowManagementOS $true
 New-Item $VMStuff\Lab -ItemType Directory
 New-Item $VMStuff\ISOs -ItemType Directory
@@ -9,8 +11,6 @@ Set-Location $VMStuff\ISOs
 Invoke-WebRequest -Uri "https://software-static.download.prss.microsoft.com/sg/download/888969d5-f34g-4e03-ac9d-1f9786c66749/SERVER_EVAL_x64FRE_en-us.iso" -OutFile "$ISOSavePath"
 
 Write-Host "Grab a x64 ISO from https://www.microsoft.com/en-us/evalcenter/download-windows-server-2022 and save it in the ISOs folder."
-
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
 Install-Module -Name Convert-WindowsImage
 

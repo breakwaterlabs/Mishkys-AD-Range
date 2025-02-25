36f86fa0559c448a4e2e72686586a272af562bef
breakwaterlabs <John@breakwaterlabs.net>
2025-02-25 15:19:03 -0500

    avoid re-downloading unnecessarily asscript may need to be re-run elevated and we don't want to trigger an overwrite / re-download of 5GB for no reason

--- Pre-reqs.ps1
+++ Pre-reqs.ps1
@@ -19,7 +19,10 @@ New-Item $VMStuff\Lab -ItemType Directory
 New-Item $VMStuff\ISOs -ItemType Directory
 Set-Location $VMStuff\ISOs
 
-Invoke-WebRequest -Uri "https://software-static.download.prss.microsoft.com/sg/download/888969d5-f34g-4e03-ac9d-1f9786c66749/SERVER_EVAL_x64FRE_en-us.iso" -OutFile "$ISOSavePath"
+# avoid re-downloading unnecessarily asscript may need to be re-run elevated and we don't want to trigger an overwrite / re-download of 5GB for no reason
+if (-not (test-path $isoSavePath)) {
+    Invoke-WebRequest -Uri "https://software-static.download.prss.microsoft.com/sg/download/888969d5-f34g-4e03-ac9d-1f9786c66749/SERVER_EVAL_x64FRE_en-us.iso" -OutFile $IsoSavePath
+}
 
 Write-Host "Grab a x64 ISO from https://www.microsoft.com/en-us/evalcenter/download-windows-server-2022 and save it in the ISOs folder."
 
